2470d059a07ec6af94d6dcaf1b491d05c42f8bdd
muslik98 <muslikhudinm@gmail.com>
2023-04-02 16:18:30 +0700

    faq style update

--- script.js
+++ script.js
@@ -2,21 +2,17 @@ const svgs = document.querySelectorAll(".reveal");
 const headerP = document.querySelectorAll(".text-header p");
 svgs.forEach(function(svg) {
     // add a click event listener to each SVG element
-    svg.addEventListener("click", function(a) {
-      // get the next sibling element with class "hidden-text"
-      const hiddenText = this.parentNode.nextElementSibling;
-      const parentNode = this.parentNode.parentNode;
+    svg.addEventListener("click", function() {
+      const hiddenText = this.parentNode.parentNode.querySelector(".hidden-text");
+      let textHeader = this.parentNode.querySelector("p");
 
-      let pHeader = parentNode.querySelector("p");
-      let bElement = document.createElement("b");
-      bElement.innerHTML = pHeader.innerHTML;
-      pHeader.replaceWith(bElement);
-      console.log(bElement.innerHTML);
-
-      hiddenText.style.display = 'block';
-      // toggle the "hidden" class to show/hide the hidden text
-      hiddenText.classList.toggle("hidden");
-      // toggle the "active" class to highlight the SVG as active/inactive
-      this.classList.toggle("active");
+      if (hiddenText.style.display === "none") {
+        hiddenText.style.display = "block";
+        textHeader.style.fontWeight = "bold";
+      } else {
+        hiddenText.style.display = "none";
+        textHeader.style.fontWeight = "normal";
+      }
     });
+    
   });
